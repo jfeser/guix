d5177f8cae3996503efd175c1b92d17b73cf9d69
Jack Feser <feser@mit.edu>
2022-07-21 11:43:26 -0400

    set SKETCH_HOME when installing sketch

--- feser/sketch.scm
+++ feser/sketch.scm
@@ -8,14 +8,15 @@
   #:use-module (gnu packages bison)
   #:use-module (gnu packages flex))
 
-(define-public sketch
+(define-public sketch-binary
   (package
     (name "sketch")
     (version "1.7.6")
+    (home-page "https://people.csail.mit.edu/asolar")
     (source
      (origin
        (method url-fetch)
-       (uri (string-append "https://people.csail.mit.edu/asolar/sketch-" version ".tar.gz"))
+       (uri (string-append home-page "/sketch-" version ".tar.gz"))
        (sha256 (base32 "01x75pkxlv74s1lscdd6w1nxzjrq3av8rbgrbwhk5m8zhk7cknjw"))))
     (build-system gnu-build-system)
     (arguments
@@ -28,18 +29,18 @@
            (lambda* (#:key outputs #:allow-other-keys)
              (chdir "../sketch-frontend")
              (let* ((out (assoc-ref outputs "out")))
-               (install-file "sketch" (string-append out "/share/sketch"))
-               (install-file (string-append "sketch-" ,version "-noarch.jar") (string-append out "/share/sketch"))
+               (install-file "sketch" (string-append out "/bin"))
+               (install-file (string-append "sketch-" ,version "-noarch.jar") (string-append out "/bin"))
                (copy-recursively "runtime" (string-append out "/share/sketch/runtime"))
                (copy-recursively "sketchlib" (string-append out "/share/sketch/sketchlib"))))))))
     (native-inputs
      (list bison flex))
     (inputs
      (list openjdk17))
-    (synopsis "Hello, Guix world: An example custom Guix package")
-    (description
-     "GNU Hello prints the message \"Hello, world!\" and then exits.  It
-serves as an example of standard GNU coding practices.  As such, it supports
-command-line arguments, multiple languages, and so on.")
-    (home-page "https://www.gnu.org/software/hello/")
+    (search-paths
+     (list (search-path-specification
+            (variable "SKETCH_HOME")
+            (files (list "share/sketch/runtime")))))
+    (synopsis "Sketch is a tool for program synthesis by sketching")
+    (description #f)
     (license license:expat)))
