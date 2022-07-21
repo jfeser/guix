3b33b49f27423ed49dd4855af948a00ac37bb5a2
Jack Feser <feser@mit.edu>
2022-07-21 13:19:35 -0400

    add more core packages

--- feser/ocaml.scm
+++ feser/ocaml.scm
@@ -77,7 +77,7 @@
     (properties `((upstream-name . "expect_test_helpers_core")))
     (synopsis "Helpers for writing expectation tests")
     (description
-     " This library provides helper functions for writing expect tests.
+     "This library provides helper functions for writing expect tests.
 
 If you want helpers for writing expect tests using the Async library, look at
 expect_test_helpers_async.")
@@ -114,7 +114,7 @@ expect_test_helpers_async.")
     (properties `((upstream-name . "core_unix")))
     (synopsis "Unix-specific portions of Core")
     (description
-     " Unix-specific extensions to some of the modules defined in [core] and
+     "Unix-specific extensions to some of the modules defined in [core] and
 [core_kernel].")
     (license license:expat)))
 
@@ -329,9 +329,7 @@ the constraints imposed by the structure of the document and by the text width."
     (build-system dune-build-system)
     (propagated-inputs (list ocaml-cppo ocaml-pprint ocaml-seq))
     (synopsis "An efficient implementation of ephemeral and persistent sequences")
-    (description
-     "This package lacks a description.  Run \"info '(guix) Synopses and Descriptions'\"
-for more information.")
+    (description #f)
     (license license:lgpl3+)))
 
 (define ocaml-ppx-yojson-conv-lib
@@ -423,10 +421,9 @@ for more information.")
      `(#:tests? #f))
     (propagated-inputs (list ocaml-result ocaml-seq dune-configurator))
     (synopsis
-     "Simple abstraction over `iter` functions, intended to iterate efficiently on collections while performing some transformations")
-    (description
-     "This package lacks a description.  Run \"info '(guix) Synopses and Descriptions'\"
-for more information.")
+     "Simple abstraction over `iter` functions, intended to iterate efficiently on
+collections while performing some transformations")
+    (description #f)
     (license #f)))
 
 
@@ -630,3 +627,77 @@ guarantee.")))
     (synopsis
      "PPX syntax for Lwt, providing something similar to async/await from JavaScript")
     (description #f)))
+
+(define-public ocaml-textutils-kernel
+  (package
+    (name "ocaml-textutils-kernel")
+    (version "0.15.0")
+    (source (origin
+              (method url-fetch)
+              (uri
+               "https://ocaml.janestreet.com/ocaml-core/v0.15/files/textutils_kernel-v0.15.0.tar.gz")
+              (sha256
+               (base32
+                "0dzsazmwnjq11z533wq3a319drlgrjgxr28y2j40h2i4qdfr3b5c"))))
+    (build-system dune-build-system)
+    (arguments
+     `(#:test-target "."))
+    (propagated-inputs (list ocaml-core ocaml-ppx-jane ocaml-uutf))
+    (properties `((upstream-name . "textutils_kernel")))
+    (home-page "https://github.com/janestreet/textutils_kernel")
+    (synopsis "Text output utilities")
+    (description "")
+    (license license:expat)))
+
+(define-public ocaml-textutils
+  (package
+    (name "ocaml-textutils")
+    (version "0.15.0")
+    (source (origin
+              (method url-fetch)
+              (uri
+               "https://ocaml.janestreet.com/ocaml-core/v0.15/files/textutils-v0.15.0.tar.gz")
+              (sha256
+               (base32
+                "1pr2vhvmiqcdipwd21vif24p2zzv6kbhvcmzkzb7qwhqmnv8ydn0"))))
+    (build-system dune-build-system)
+    (arguments
+     `(#:test-target "."))
+    (propagated-inputs (list ocaml-core
+                             ocaml-core-kernel
+                             ocaml-core-unix
+                             ocaml-ppx-jane
+                             ocaml-textutils-kernel
+                             ocaml-uutf))
+    (home-page "https://github.com/janestreet/textutils")
+    (synopsis "Text output utilities")
+    (description "")
+    (license license:expat)))
+
+(define-public ocaml-core-bench
+  (package
+    (name "ocaml-core-bench")
+    (version "0.15.0")
+    (source (origin
+              (method url-fetch)
+              (uri
+               "https://ocaml.janestreet.com/ocaml-core/v0.15/files/core_bench-v0.15.0.tar.gz")
+              (sha256
+               (base32
+                "0n2jaf62l7i5k48rm1z1kihypaq1qz3dyplda8fvmw1mbgkn9csx"))))
+    (build-system dune-build-system)
+    (propagated-inputs (list ocaml-core
+                             ocaml-core-kernel
+                             ocaml-core-unix
+                             ocaml-ppx-compare
+                             ocaml-ppx-fields-conv
+                             ocaml-ppx-jane
+                             ocaml-ppx-let
+                             ocaml-ppx-sexp-conv
+                             ocaml-textutils
+                             ocaml-re))
+    (properties `((upstream-name . "core_bench")))
+    (home-page "https://github.com/janestreet/core_bench")
+    (synopsis "Benchmarking library")
+    (description "")
+    (license license:expat)))
